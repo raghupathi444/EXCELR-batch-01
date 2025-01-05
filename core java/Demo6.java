8fadd57d9034465ed7ae60d33ade18ddff0b55d7
raghupathi444 <akarapuraghupathi@gmail.com>
2025-01-05 20:29:18 +0530

    Add files via upload
    Demo6

---
+++ core java/Demo6.java
@@ -0,0 +1,11 @@
+public class Demo6 {
+    public static void main(String []args) {
+        String username = "Raghupathi";
+        String pwd = "raghupathi@123";
+        while((username == "Raghupathi") && (pwd == "raghupathi@123")) {
+            System.out.println("hello  Raghupathi welcome to malla reddy university");
+            break;
+        }
+    }
+}
+
