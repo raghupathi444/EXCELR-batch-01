a958b8569ade1ca0c9ec01cd1e2a1e13300fede5
Raghupathi <akarapuraghupathi@gmail.com>
2025-01-22 19:17:44 +0530

    Add files via upload
    Hello World

--- core java/Demo001.java
+++ core java/Demo001.java
@@ -1,5 +1,5 @@
-public class Demo1{
+public class Demo001 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello, World!");
     }
 }
