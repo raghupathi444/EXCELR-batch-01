55154865e78e78cf08171b7532401b43846e90b7
Raghupathi <akarapuraghupathi@gmail.com>
2025-01-22 20:46:25 +0530

    Diff Username & Password

---
+++ core java/Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String []args) {
+        String username = "Raghupathi";
+        String pwd = "raghupathi@123";
+        while((username == "Raghupathi") && (pwd == "raghupathi@123")) {
+            System.out.println("hello  Raghupathi welcome to malla reddy university");
+            break;
+        }
+    }
+}
