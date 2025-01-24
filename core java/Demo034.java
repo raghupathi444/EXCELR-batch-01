8883d6cdbcec66f259958a36fc23c4469b870e23
Raghupathi <akarapuraghupathi@gmail.com>
2025-01-24 21:53:43 +0530

    EvenOddChecker

---
+++ core java/Demo034.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Demo034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        System.out.println(num % 2 == 0 ? "Even" : "Odd");
+        sc.close(); 
+    }
+}
+
