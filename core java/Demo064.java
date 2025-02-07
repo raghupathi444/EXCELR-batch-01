109c26ff70983c2930c5663e272cadc6869cd8eb
Raghupathi <akarapuraghupathi@gmail.com>
2025-02-07 08:56:56 +0530

    Swapping first and last elements

---
+++ core java/Demo064.java
@@ -0,0 +1,13 @@
+public class Demo064 {
+        public static void main(String[] args) {
+            int[] arr = {10, 20, 30, 40, 50};
+    
+            int temp = arr[0];
+            arr[0] = arr[arr.length - 1];
+            arr[arr.length - 1] = temp;
+    
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+    }
